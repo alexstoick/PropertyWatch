49fe7b50180e29bf19c0b2cf3912b5e7a6624359
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-12-29 12:31:51 +0200

    added zone model

---
+++ PropertyWatch/Zone.h
@@ -0,0 +1,16 @@
+//
+//  Zone.h
+//  PropertyWatch
+//
+//  Created by Stoica Alexandru on 12/29/13.
+//  Copyright (c) 2013 Stoica Alexandru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Zone : NSObject
+
+@property (assign,nonatomic) NSInteger id ;
+@property (strong,nonatomic) NSString * postcode ;
+
+@end
---
+++ PropertyWatch/Zone.m
@@ -0,0 +1,13 @@
+//
+//  Zone.m
+//  PropertyWatch
+//
+//  Created by Stoica Alexandru on 12/29/13.
+//  Copyright (c) 2013 Stoica Alexandru. All rights reserved.
+//
+
+#import "Zone.h"
+
+@implementation Zone
+
+@end
